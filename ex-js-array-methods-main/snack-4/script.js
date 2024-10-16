69d7f5ba0488748ec108eb34e67e32231ac5fd79
martinasartori97 <martinasartori9@gmail.com>
2024-10-16 15:32:19 +0200

    edit js

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,12 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map(post => post.author);
+console.log(authors);
+
+for (let i = 0; i < posts.length; i++) {
+  authors.push(posts[i].author);
+}
+
+console.log(authors);
